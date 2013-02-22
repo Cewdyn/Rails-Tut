9c4ab07c4a445d26f9564174defb34cdc13231c4
Dan Brown <dan@dan-N80Vb-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2013-02-22 01:08:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'eb8b11c1292d452061c6c1177d66948c46c3d93daebc29cded6a5abf277d6f15afa0025898a63057ad517ee9d7854c8c5ed6f9d3835087699d0611192c534db9'
